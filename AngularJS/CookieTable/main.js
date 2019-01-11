a2c6f42b5b0d142ba97af074ab2620fed7b9a705
Kis Zoltán Csaba <spydertuning@citromail.hu>
2019-01-11 11:59:50 +0100

    added new features

--- AngularJS/CookieTable/main.js
+++ AngularJS/CookieTable/main.js
@@ -12,27 +12,39 @@ app.controller('cookieCtrl', function($scope, $cookies){
         console.log($scope.table) 
     };
 
+    //add new data
+    $scope.new = function(){
+        var data = {
+            name : "",
+            email : "",
+            password : ""
+        };
+        $scope.table.splice($scope.table.length, 0, data);
+        $scope.modify($scope.table.length-1);
+    };
+
+    //modify data
     $scope.modify = function(i){
         $scope.userName = $scope.table[i].name;
         $scope.userMail = $scope.table[i].email;
         $scope.userPassword = $scope.table[i].password;
     
         $scope.save = function(){
-            var data={        
-            };
+            var data = {};
             data.name = $scope.userName;
             data.email = $scope.userMail;
             data.password = $scope.userPassword;
             $scope.table.splice(i, 1, data);
-            //console.log($scope.table);
             $scope.myJSON = JSON.stringify($scope.table); 
-            //console.log($scope.myJSON);
             $cookies.put("cookie", $scope.myJSON);
         };
     };
 
-    
     $scope.delete = function (i) {
         $scope.table.splice(i, 1);
+        $scope.myJSON = JSON.stringify($scope.table); 
+        $cookies.put("cookie", $scope.myJSON);
     }
+
+
 });
